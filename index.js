53536d5d284f0a54d9703e1ac661ab7bd5e4483b
Mathews <mathews.milimu@student.moringaschool.com>
2022-08-20 11:24:55 +0300

    Lab done

---
+++ index.js
@@ -0,0 +1,22 @@
+
+
+function receivesAFunction(spy) {
+    spy ();
+}
+spy();
+
+function returnsANamedFunction () {
+    let workout = function () { 
+
+    }
+   return returnsANamedFunction   
+
+}
+
+function returnsAnAnonymousFunction () {
+    return function () { 
+
+    }
+   
+
+}
